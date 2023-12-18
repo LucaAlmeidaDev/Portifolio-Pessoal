4c60eaea224d874eebf8373cf21f4b096558aba8
Lucas Almeida <124281689+LucaAlmeidaDev@users.noreply.github.com>
2023-12-18 18:44:32 -0300

    0.1

--- src/components/Toggle/index.jsx
+++ src/components/Toggle/index.jsx
@@ -1,6 +1,32 @@
 import './toggle.css';
 
 function Toggle(){
+    const modeImage = document.getElementById('modeImage');
+
+    let darkMode = false;
+
+    const darkModeButton = document.getElementById('darkModeButton');
+    const darkModeIcon = document.getElementById('darkModeIcon');
+    const lightModeIcon = document.getElementById('lightModeIcon');
+
+    darkModeButton.addEventListener('click', toggleDarkMode);
+
+    function toggleDarkMode() {
+        darkMode = !darkMode;
+        if (darkMode) {
+            darkModeIcon.style.display = 'none';
+            lightModeIcon.style.display = 'block';
+            document.documentElement.style.setProperty('--background-color', '#171717');
+            document.documentElement.style.setProperty('--titulo-color', '#FFF');
+            document.documentElement.style.setProperty('--resumo-text', '#A1A6AD');
+        } else {
+            lightModeIcon.style.display = 'none';
+            darkModeIcon.style.display = 'block';
+            document.documentElement.style.setProperty('--background-color', '#FFF');
+            document.documentElement.style.setProperty('--titulo-color', '#000');
+            document .documentElement.style.setProperty('--resumo-text', '#0A2942');
+        }
+    }
     return(
         <div>
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,10 +2,18 @@ import './home.css';
 
 function Home(){
     return(
-        <div>
-            <h1>Teste</h1>
+        <div className='hero' id='home'>
+                <div class="hero-text">
+                    <h1>Lucas Almeida</h1>
+                    <h2>Fullstack Developer</h2>
+                    <p>Eu sou um desenvolvedor Full Stack com conhecimento em React.Js, Node.Js, 
+                        Java & Python e com o maior foco criações criativas, rapidas, responsivas
+                        e de conforto para o úsuario. Apesar de relativamente novo na área dedico 
+                        todo o meu tempo para me manter atualizado.</p>   
+                    <a class="btn" href="Assets/github.png" download="github teste" >Download CV<i class="ri-download-line"></i></a>
+                </div>
         </div>
     );
-};
+};  
 
 export default Home;
